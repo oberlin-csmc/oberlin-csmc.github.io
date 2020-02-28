4d0ef862408a52f0978e517402c32ef5e74b5c95
janeon <jhsieh@oberlin.edu>
2020-02-27 23:23:49 -0500

    json lists

--- tabs/test.js
+++ tabs/test.js
@@ -1,9 +1,9 @@
 $.getJSON( "test.json", function( json ) {
     // console.log( "JSON Data received, name is " + JSON.stringify(json, null, "    "));
     for (var year of json) {
-      console.log("year" + year.year)
+      console.log("year: " + year.year)
       for (var member in year.members) {
-        console.log(member)
+        console.log(year.members[member])
       }
     }
 });
