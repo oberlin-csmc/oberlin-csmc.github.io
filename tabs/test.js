8e0f87ff8f3bd6f0b5cca72edac7649ef0d5b53c
janeon <jhsieh@oberlin.edu>
2020-02-28 00:46:55 -0500

    members

--- tabs/test.js
+++ tabs/test.js
@@ -1,6 +1,7 @@
 $.getJSON( "test.json", function( json ) {
     // console.log( "JSON Data received, name is " + JSON.stringify(json, null, "    "));
     var outer = document.createElement("div");
+    outer.setAttribute("id", "members");
     for (var year of json) {
       // console.log("year: " + year.year)
       var year_node = document.createElement("div")
@@ -8,10 +9,11 @@ $.getJSON( "test.json", function( json ) {
       var year_text = document.createTextNode(year.year);
       year_header.appendChild(year_text);
       year_node.appendChild(year_header);
+      for (var indx in year.members) {
+        var member = year.members[indx]
+          year_node.append(document.createElement(member.name))
+      }
       outer.append(year_node)
-      // for (var indx in year.members) {
-      //   var member = year.members[indx]
-      // }
     }
     document.body.appendChild(outer);
 });
