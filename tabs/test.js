93f0196e3bafae0e1039454422ea387e50224992
janeon <jhsieh@oberlin.edu>
2020-02-28 00:35:18 -0500

    adding node

--- tabs/test.js
+++ tabs/test.js
@@ -2,9 +2,13 @@ $.getJSON( "test.json", function( json ) {
     // console.log( "JSON Data received, name is " + JSON.stringify(json, null, "    "));
     var outer = document.createElement("div");
     for (var year of json) {
-      console.log("year: " + year.year)
-      var year_node = document.createTextNode(year.year);
-      outer.appendChild(year_node);
+      // console.log("year: " + year.year)
+      var year_node = document.createElement("div")
+      var year_header = document.createElement("H3")
+      var year_text = document.createTextNode(year.year);
+      year_header.appendChild(year_text);
+      year_node.appendChild(year_header);
+      outer.append(year_node)
       // for (var indx in year.members) {
       //   var member = year.members[indx]
       // }
