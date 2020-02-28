dad282d183a04cdb6e6223032fcc042e2cf917dd
janeon <jhsieh@oberlin.edu>
2020-02-27 22:52:21 -0500

    attempt printing json parsed object

--- tabs/test.js
+++ tabs/test.js
@@ -1,3 +1,3 @@
 $.getJSON( "test.json", function( json ) {
-    console.log( "JSON Data received, name is " + json);
+    console.log( "JSON Data received, name is " + JSON.stringify(data, null, "    "));
 });
