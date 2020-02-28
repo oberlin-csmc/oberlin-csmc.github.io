e2c9da6fff3da76b6ccab4609d3779e703f77df4
janeon <jhsieh@oberlin.edu>
2020-02-27 23:19:55 -0500

    testing officers json

--- tabs/test.js
+++ tabs/test.js
@@ -1,3 +1,9 @@
 $.getJSON( "test.json", function( json ) {
-    console.log( "JSON Data received, name is " + JSON.stringify(json, null, "    "));
+    // console.log( "JSON Data received, name is " + JSON.stringify(json, null, "    "));
+    for (var year of json) {
+      console.log("year" + year.year)
+      for (var member in year.members) {
+        console.log(member)
+      }
+    }
 });
